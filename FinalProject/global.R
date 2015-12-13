64bf11451ace06a6ac2f770ee1f8af2d0aa50352
JIE SUN <js641@duke.edu>
2015-12-12 21:07:50 -0500

    Delete global.R

--- FinalProject/global.R
+++
@@ -1,8 +0,0 @@
-requestURL <- "https://api.twitter.com/oauth/request_token"
-accessURL <- "http://api.twitter.com/oauth/access_token"
-authURL <- "http://api.twitter.com/oauth/authorize"
-consumerKey <- "zSeAWHNpaL5G7GpHuCO4zTffT"
-consumerSecret <- "dZnarPgWiQCnb1bJ0tvN3xFBmWVMRQCDDWl9UsFtkiTGpzztfG"
-my_oauth <- OAuthFactory$new(consumerKey=consumerKey,
-                             consumerSecret=consumerSecret, requestURL=requestURL,
-                             accessURL=accessURL, authURL=authURL)
